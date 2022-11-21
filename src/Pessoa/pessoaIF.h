84e545d6d25e2b03bff6ff0cb5bc08dd94d40695
Guilherme Figueiredo <guilhermefigueiredo341@gmail.com>
2022-11-21 13:38:52 -0300

    reparos na classe: PessoaIF

---
+++ src/Pessoa/pessoaIF.h
@@ -0,0 +1,32 @@
+#ifndef PESSOA_IF_H
+#define PESSOA_IF_H
+
+#include <string>
+#include <vector>
+
+class PessoaIF
+{
+private:
+    static int _numPessoa;
+    int _id;
+    std::string _nome;
+    std::string _numIdentidade;
+    int _dia;
+    int _mes;       // arrumar para o tipo data 
+    int _ano;
+
+public:
+    ~PessoaIF() {};
+
+    virtual int getPessoaById() const = 0;
+    virtual int get_id() = 0;
+    virtual std::string get_nome() = 0;
+    virtual std::string get_numIdentidade() = 0;
+    virtual int get_dia() = 0;
+    virtual int get_mes() = 0;
+    virtual int get_ano() = 0;
+
+    virtual int set_nome() = 0;
+};
+
+#endif
