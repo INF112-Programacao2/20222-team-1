6fb3b7ceb6e102df736ada2fe37f684d3ce3ae3b
Guilherme Figueiredo <guilhermefigueiredo341@gmail.com>
2022-11-21 13:23:38 -0300

    pessoa -> interface pessoa

--- src/Pessoa/pessoa.h
+++ src/Pessoa/pessoa.h
@@ -4,21 +4,25 @@
 class Pessoa
 {
 private:
+    static int _numPessoa;
+    int _id
     std::string _nome;
     std::string _numIdentidade;
     int _dia;
-    int _mes;
+    int _mes;       // arrumar para o tipo data 
     int _ano;
 
 public:
     Pessoa(std::string nome, std::string numIdentidade, int dia, int mes, int ano);
-    ~Pessoa();
+    ~Pessoa() {};
 
-    std::string get_nome();
-    std::string get_numIdentidade() const;
-    int get_dia();
-    int get_mes();
-    int get_ano();
+    int getPessoaById() const = 0;
+    int get_id() = 0;
+    std::string get_nome() = 0;
+    std::string get_numIdentidade() = 0;
+    int get_dia() = 0;
+    int get_mes() = 0;
+    int get_ano() = 0;
 
-    int set_nome();
+    int set_nome() = 0;
 };
