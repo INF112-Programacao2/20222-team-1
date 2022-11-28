28937701e2b7ee1da636eea4d0ed8f833ec3e48c
Luiz Felipe Vicente Da Costa <fc671219@gmail.com>
2022-11-27 23:06:19 -0300

    Formulario de login.

--- src/Graphics/Login/login.cpp
+++ src/Graphics/Login/login.cpp
@@ -1,14 +1,13 @@
 #include "login.h"
 #include "ui_login.h"
-#include <QPixmap>
 
 Login::Login(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Login)
 {
     ui->setupUi(this);
-    //QPixmap logo("./assets/doesangue_splash_logo.png");
-    //ui->logo_img->setPixmap(logo);
+    ui->frSelectTypeUser->setVisible(false);
+    ui->frLogarForm->setVisible(true);
 }
 
 Login::~Login()
