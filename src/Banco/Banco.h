1486b4a20e384cd37bffb8a0a059a89361bf36f1
Lorena Souza Moreira <lorenasouzamoreiraa@gmail.com>
2022-12-10 19:28:28 -0300

    validacao login profissional

--- src/Banco/Banco.h
+++ src/Banco/Banco.h
@@ -15,32 +15,36 @@ class Banco
 {
 private:
     static Banco *_instance;
-    vector<Doador> _doadores;
-    vector<Receptor> _receptores;
-    vector<ProfissionalSaude> _profissionais;
-    vector<Sangue> _sangue;
-    vector<Doacao> _doacao;
-    vector<Consumo> _consumo;
-    vector<Instituicao> _instituicao;
+    vector<Doador*> _doadores;
+    vector<Receptor*> _receptores;
+    vector<ProfissionalSaude*> _profissionais;
+    vector<Sangue*> _sangue;
+    vector<Doacao*> _doacao;
+    vector<Consumo*> _consumo;
+    vector<Instituicao*> _instituicao;
     Banco();
 
 public:
     Banco* getInstance();
     ~Banco();
 
-    void setReceptor(Receptor receptor);
-    void setDoador(Doador doador);
-    void setProfissional(ProfissionalSaude profissional);
-    void setDoador(Doador doador);
-    void setConsumo(Consumo consumo);
-    void setInstituicao(Instituicao instituicao);
+    void setReceptor(Receptor* receptor);
+    void setDoador(Doador* doador);
+    void setProfissional(ProfissionalSaude* profissional);
+    void setDoador(Doador* doador);
+    void setConsumo(Consumo* consumxo);
+    void setInstituicao(Instituicao* instituicao);
 
-    Receptor getReceptorById(int id);
-    Doador getDoadorById(int id);
-    ProfissionalSaude getProfissionalById(int id);
-    Doador getDoadorById(int id);
-    Consumo getConsumoById(int id);
-    Instituicao getInstituicaoById(int id);
+    Receptor* getReceptorById(int id);
+    Doador* getDoadorById(int id);
+    ProfissionalSaude* getProfissionalById(int id);
+    Doador* getDoadorById(int id);
+    Consumo* getConsumoById(int id);
+    Instituicao* getInstituicaoById(int id);
+
+    ProfissionalSaude* isProfissional(std::string cpf);
+    Doador* isDoador(std::string cpf);
+    Receptor* isReceptor(std::string cpf);
 
     vector<Doacoes> getDoacoesCompativeis(int idSangue);
 };
--- src/Graphics/Login/login.cpp
+++ src/Graphics/Login/login.cpp
@@ -1,9 +1,38 @@
 #include "login.h"
 #include "ui_login.h"
+#include "../Banco/Banco.h"
 
-Login::Login(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::Login)
+Banco i;
+
+bool isWord(std::string palavra){
+    for(int i =0; i< palavra.size(); i++){    
+        if(isdigit(palavra[i]))
+               throw std::invalid_argument("/!\\A palavra digitada deve conter apenas letras. ");    
+    }
+    return true;
+}
+
+bool isCpf(std::string palavra){
+    if(palavra.size()==14){
+        for(int i=0; i< palavra.size(); i++){     
+            if(palavra[i]!='.' && (i==3 || i==7))
+                throw std::invalid_argument("/!\\Formato invalido de CPF. Verifique os '.'. "); 
+            else if(palavra[i]!='-' && i== 11 )
+                throw std::invalid_argument("/!\\Formato invalido de CPF. Verifique os '-'. ");    
+            else if(!isdigit(palavra[i]) && palavra[i]!='-' && palavra[i]!='.' && palavra[i]!='/'){
+                std::cout<<i<<":"<<palavra[i]<<std::endl;
+                throw std::invalid_argument("/!\\O CPF e composto apenas de numeros. Nao possui letras. "); 
+
+            } 
+        }
+    }
+    else
+        throw std::out_of_range("/!\\O CPF deve possui 14 caracteres( verifique a pontuação). "); 
+    return 0;   
+}
+
+
+Login::Login(QWidget *parent) : QMainWindow(parent), ui(new Ui::Login)
 {
     ui->setupUi(this);
     ui->frSelectTypeUser->setVisible(true);
@@ -30,6 +59,27 @@ void Login::on_btInstituicaoLogin_clicked()
 
 void Login::on_btProfissionalSaudeLogin_clicked()
 {
+    try{
+        std::string login = ui->lineEdit->text();
+        if(isCpf(login)){
+            ProfissionalSaude* user = i.isProfissional(login);
+            if(user != nullptr){
+                if(user->get_senha() == ui->lineEdit2->text())
+                    std::cout<<"bonaceta natasha";
+                else
+                    throw std::out_of_range("Verifique sua senha");
+            }else
+                throw std::exception("Não existe nenhuma conta com essas credenciais.");
+        }
+
+    }catch(std::invalid_argument &e){
+        QMessageBox::information(this, "Atencao", e,what());
+    }catch(std::out_of_range &e){
+        QMessageBox::information(this, "Atencao", e,what());
+    }catch(std::exception &e){
+        QMessageBox::information(this, "Atencao", e,what());
+    }
+
     ui->frSelectTypeUser->setVisible(false);
     ui->frLogarForm->setVisible(true);
 }
