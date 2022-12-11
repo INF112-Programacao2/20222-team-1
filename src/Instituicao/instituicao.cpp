cbb1380f9143ae466cdd159d9e7cf9cfc27d1306
erick <erick.s.souza@ufv.br>
2022-12-11 19:08:12 -0300

    correção de erros

--- src/Instituicao/instituicao.cpp
+++ src/Instituicao/instituicao.cpp
@@ -6,15 +6,16 @@
 #include "../ProfissionalSaude/profissionalSaude.h"
 #include "../Banco/Banco.h"
 
-Banco* binstituicao = new Banco(); 
+Banco *binstituicao = new Banco();
 
 int Instituicao::_numInstituicao = 0;
 
 Instituicao::~Instituicao() {}
 
-Instituicao::Instituicao(std::string nome, std::string endereco, std::string cnpj, std::string senha) {
+Instituicao::Instituicao(std::string nome, std::string endereco, std::string cnpj, std::string senha)
+{
     _id = _numInstituicao;
-    _numInstituicao ++;
+    _numInstituicao++;
     _nome = nome;
     _endereco = endereco;
     _cnpj = cnpj;
@@ -23,7 +24,8 @@ Instituicao::Instituicao(std::string nome, std::string endereco, std::string cnp
     _numInstituicao++;
 }
 
-Instituicao::Instituicao(int id, std::string nome, std::string endereco, std::string cnpj, std::string senha) {
+Instituicao::Instituicao(int id, std::string nome, std::string endereco, std::string cnpj, std::string senha)
+{
     _id = id;
     _nome = nome;
     _endereco = endereco;
@@ -33,34 +35,41 @@ Instituicao::Instituicao(int id, std::string nome, std::string endereco, std::st
     _numInstituicao++;
 }
 
-std::string Instituicao::get_nome() {
+std::string Instituicao::get_nome()
+{
     return _nome;
 }
 
-std::string Instituicao::get_senha(){
+std::string Instituicao::get_senha()
+{
     return _senha;
 }
-std::string Instituicao::get_endereco() {
+std::string Instituicao::get_endereco()
+{
     return _endereco;
 }
 
-void Instituicao::set_nome(std::string nome) {
+void Instituicao::set_nome(std::string nome)
+{
     _nome = nome;
 }
 
-void Instituicao::set_static(int id) {
+void Instituicao::set_static(int id)
+{
     Instituicao::_numInstituicao = id;
 }
 
-void  Instituicao::set_endereco(std::string endereco) {
+void Instituicao::set_endereco(std::string endereco)
+{
     _endereco = endereco;
 }
 
-int Consumo::get_id()
+int Instituicao::get_id()
 {
     return _id;
 }
 
-void Instituicao::cadastrarProfissional(std::string nome, std::string cpf, struct tm *data, std::string senha, Cargo cargo) {
-    binstituicao->cadastrarProfissional(new ProfissionalSaude( nome, cpf, data, senha, cargo, _id));
+void Instituicao::cadastrarProfissional(std::string nome, std::string cpf, struct tm *data, std::string senha, Cargo cargo)
+{
+    binstituicao->cadastrarProfissional(new ProfissionalSaude(nome, cpf, data, senha, cargo, _id));
 }
