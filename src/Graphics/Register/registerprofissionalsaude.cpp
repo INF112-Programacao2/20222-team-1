e89ecb71507b482db581649a126d977a31fe8e27
Luiz Felipe Vicente Da Costa <fc671219@gmail.com>
2022-12-10 21:52:18 -0300

    Criar formulário de cadastro de profissional de saúde.

---
+++ src/Graphics/Register/registerprofissionalsaude.cpp
@@ -0,0 +1,26 @@
+#include "registerprofissionalsaude.h"
+#include "ui_registerprofissionalsaude.h"
+
+RegisterProfissionalSaude::RegisterProfissionalSaude(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::RegisterProfissionalSaude)
+{
+    ui->setupUi(this);
+}
+
+RegisterProfissionalSaude::~RegisterProfissionalSaude()
+{
+    delete ui;
+}
+
+void RegisterProfissionalSaude::on_buttonReturn_clicked()
+{
+
+}
+
+
+void RegisterProfissionalSaude::on_buttonRegistrar_clicked()
+{
+
+}
+
---
+++ src/Graphics/Register/registerprofissionalsaude.h
@@ -0,0 +1,27 @@
+#ifndef REGISTERPROFISSIONALSAUDE_H
+#define REGISTERPROFISSIONALSAUDE_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class RegisterProfissionalSaude;
+}
+
+class RegisterProfissionalSaude : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit RegisterProfissionalSaude(QWidget *parent = nullptr);
+    ~RegisterProfissionalSaude();
+
+private slots:
+    void on_buttonReturn_clicked();
+
+    void on_buttonRegistrar_clicked();
+
+private:
+    Ui::RegisterProfissionalSaude *ui;
+};
+
+#endif // REGISTERPROFISSIONALSAUDE_H
