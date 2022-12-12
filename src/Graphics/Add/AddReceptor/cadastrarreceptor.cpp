6d230a23a10d6460a43d99ba38bfffacc85de2f8
Guilherme Figueiredo <guilhermefigueiredo341@gmail.com>
2022-12-12 01:01:45 -0300

    Navegar entre telas.

--- src/Graphics/Add/AddReceptor/cadastrarreceptor.cpp
+++ src/Graphics/Add/AddReceptor/cadastrarreceptor.cpp
@@ -12,3 +12,16 @@ CadastrarReceptor::~CadastrarReceptor()
 {
     delete ui;
 }
+
+void CadastrarReceptor::on_buttonReturn_clicked()
+{
+    this->close();
+}
+
+
+void CadastrarReceptor::on_buttonAdd_clicked()
+{
+    // validar e cadastrar dados
+    this->close();
+}
+
