5613d73076dc9b8c71cd03111cf67b4c5dece6e6
Lorena Souza Moreira <lorenasouzamoreiraa@gmail.com>
2022-12-12 07:39:20 -0300

    editProfissional

--- src/Graphics/Edit/EditProfissional/editprofissional.cpp
+++ src/Graphics/Edit/EditProfissional/editprofissional.cpp
@@ -1,11 +1,19 @@
 #include "editprofissional.h"
 #include "ui_editprofissional.h"
+#include <QString>
+#include "../../../ProfissionalSaude/profissionalSaude.h"
+#include "../../../Banco/Banco.h"
+
+Banco* iProfissional;
 
 EditProfissional::EditProfissional(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::EditProfissional)
 {
     ui->setupUi(this);
+    ProfissionalSaude* profissional = Banco::_puser;
+    ui->inputNome->setText(QString::fromStdString((profissional->get_nome())));
+    ui->inputSenha->setText(QString::fromStdString((profissional->get_senha())));
 }
 
 EditProfissional::~EditProfissional()
